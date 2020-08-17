7c04cfe23e8c540617801a95bcf143d3dc8815ca
Devin Moore <devinmoore@google.com>
2020-08-17 14:37:13 -0700

    Removed 'flags' and 'fdIndex' fields from GrantorDescriptor
    Removed the unnecessary fields from the
    android.hardware.common.GrantorDescriptor type.
    Needed change the AidlMQDescriptorShim to hold the android::hardware
    version of GrantorDescriptor.
    Moved that android::hardware::GrantorDescriptor definition to
    MQDescriptorBase.h.
    Added some translation between to two types with some bounds checking
    for integer conversions. Added satization for intergers to the fmq
    tests.
    
    Test: atest fmq_unit_tests &&
    system/tools/hidl/tests/run_all_device_tests.sh
    
    Bug: 142326204
    Change-Id: Ie802f91b09288b3508df50b556626a592c9c2d95

--- base/fmq/MQDescriptorBase.h
+++ base/fmq/MQDescriptorBase.h
@@ -36,6 +36,20 @@ enum MQFlavor : uint32_t {
     kUnsynchronizedWrite = 0x02
 };
 
+struct GrantorDescriptor {
+    uint32_t flags __attribute__((aligned(4)));
+    uint32_t fdIndex __attribute__((aligned(4)));
+    uint32_t offset __attribute__((aligned(4)));
+    uint64_t extent __attribute__((aligned(8)));
+};
+
+static_assert(offsetof(GrantorDescriptor, flags) == 0, "wrong offset");
+static_assert(offsetof(GrantorDescriptor, fdIndex) == 4, "wrong offset");
+static_assert(offsetof(GrantorDescriptor, offset) == 8, "wrong offset");
+static_assert(offsetof(GrantorDescriptor, extent) == 16, "wrong offset");
+static_assert(sizeof(GrantorDescriptor) == 24, "wrong size");
+static_assert(__alignof(GrantorDescriptor) == 8, "wrong alignment");
+
 namespace details {
 
 typedef uint64_t RingBufferPosition;
